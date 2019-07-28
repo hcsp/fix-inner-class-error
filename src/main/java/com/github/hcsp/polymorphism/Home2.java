0567f8d22d5b15bbf7ed658739f11e7a7a17aa78
Zhang Xin <zhangxindev@gmail.com>
2019-07-28 13:29:18 +0800

    静态内部类和非静态内部类 (#17)
    静态内部类和非静态内部类 (#17)

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -8,7 +8,7 @@ public class Home2 {
     List<Cat> cats = new ArrayList<>();
 
     public List<String> getCatNames() {
-        CatNameCollector collector = new CatNameCollector();
+        CatNameCollector collector = new CatNameCollector(new Home2());
         cats.forEach(collector);
         return collector.getCatNames();
     }
@@ -23,12 +23,15 @@ public class Home2 {
         // 请思考一下为什么
         // 不要将此类改写成非静态的内部类
         // 而是引入一个外围类的实例以调用外围类的实例方法
-        // private Home2 home;
+        private Home2 home;
+        public CatNameCollector(Home2 home) {
+            this.home = home;
+        }
         private List<String> catNames = new ArrayList<>();
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            home.log(cat);
             catNames.add(cat.getName());
         }
 
