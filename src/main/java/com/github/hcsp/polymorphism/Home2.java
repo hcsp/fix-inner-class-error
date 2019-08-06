2a74101178e106417178b5c86cc3dd1057bd494b
wangpeng1994 <947034046@qq.com>
2019-08-06 16:19:42 +0800

    Fix inner class error.

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -8,7 +8,7 @@ public class Home2 {
     List<Cat> cats = new ArrayList<>();
 
     public List<String> getCatNames() {
-        CatNameCollector collector = new CatNameCollector();
+        CatNameCollector collector = new CatNameCollector(this); // 1. 这里传入外部类的实例
         cats.forEach(collector);
         return collector.getCatNames();
     }
@@ -23,12 +23,17 @@ public class Home2 {
         // 请思考一下为什么
         // 不要将此类改写成非静态的内部类
         // 而是引入一个外围类的实例以调用外围类的实例方法
-        // private Home2 home;
+        // 2. 将外部类的实例保存为自身实例成员
+         private Home2 home;
+         public CatNameCollector(Home2 home) {
+             this.home = home;
+         }
         private List<String> catNames = new ArrayList<>();
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            // 3. 通过外部类实例来调用它的实例方法
+            home.log(cat);
             catNames.add(cat.getName());
         }
 
