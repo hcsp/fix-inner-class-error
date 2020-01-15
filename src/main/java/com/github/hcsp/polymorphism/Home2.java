f8abecfa64061bd5b169c3b8b2bde79b60c35b7f
cjf <2196124754@qq.com>
2020-01-15 15:16:36 +0800

    first commit

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -25,10 +25,11 @@ public class Home2 {
         // 而是引入一个外围类的实例以调用外围类的实例方法
         // private Home2 home;
         private List<String> catNames = new ArrayList<>();
+        private Home2 home2$0 = new Home2();
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            home2$0.log(cat);
             catNames.add(cat.getName());
         }
 
