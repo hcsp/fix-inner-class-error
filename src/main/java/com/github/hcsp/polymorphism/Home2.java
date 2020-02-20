419695f447704da70c21de83ec8a088936d488a1
陈晓拉尼 <2524729719@qq.com>
2020-02-20 22:41:55 +0800

    内部类和静态类的区别 (#109)
    内部类和静态类的区别 (#109)

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -8,7 +8,7 @@ public class Home2 {
     List<Cat> cats = new ArrayList<>();
 
     public List<String> getCatNames() {
-        CatNameCollector collector = new CatNameCollector();
+        CatNameCollector collector = new CatNameCollector(this);
         cats.forEach(collector);
         return collector.getCatNames();
     }
@@ -26,9 +26,16 @@ public class Home2 {
         // private Home2 home;
         private List<String> catNames = new ArrayList<>();
 
+        private Home2 this$0;
+
+        public CatNameCollector(Home2 home) {
+            this.this$0 = home;
+
+        }
+
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            this$0.log(cat);
             catNames.add(cat.getName());
         }
 
