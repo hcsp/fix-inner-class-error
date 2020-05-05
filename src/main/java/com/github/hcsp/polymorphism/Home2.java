050cff0eec6876b388f9717d39902d7f12ec72d9
Wong <19876688427>
2020-05-05 16:17:06 +0800

    静态内部类和非静态的区别

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -24,11 +24,12 @@ public class Home2 {
         // 不要将此类改写成非静态的内部类
         // 而是引入一个外围类的实例以调用外围类的实例方法
         // private Home2 home;
+        private Home2 home;
         private List<String> catNames = new ArrayList<>();
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            home.log(cat);
             catNames.add(cat.getName());
         }
 
