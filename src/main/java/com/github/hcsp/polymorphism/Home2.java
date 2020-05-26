f7921dd39795bdd02adc92138eb16ae823518c12
Storm4542 <417002107@qq.com>
2020-05-26 16:27:29 +0800

    内部类

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -8,7 +8,7 @@ public class Home2 {
     List<Cat> cats = new ArrayList<>();
 
     public List<String> getCatNames() {
-        CatNameCollector collector = new CatNameCollector();
+        CatNameCollector collector = new CatNameCollector(this);
         cats.forEach(collector);
         return collector.getCatNames();
     }
@@ -23,12 +23,16 @@ public class Home2 {
         // 请思考一下为什么
         // 不要将此类改写成非静态的内部类
         // 而是引入一个外围类的实例以调用外围类的实例方法
-        // private Home2 home;
-        private List<String> catNames = new ArrayList<>();
 
+
+        private Home2 home;
+        private List<String> catNames = new ArrayList<>();
+        public CatNameCollector(Home2 home) {
+            this.home = home;
+        }
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            home.log(cat);
             catNames.add(cat.getName());
         }
 
