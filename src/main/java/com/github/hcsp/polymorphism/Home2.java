b6fbee95fbb58fb3996da3805ab0e1f60d5bb4dc
xu-kai-quan <50357378+xu-kai-quan@users.noreply.github.com>
2020-12-15 21:51:09 +0800

    内部类练习 (#182)
    内部类练习 (#182)

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -25,10 +25,11 @@ public class Home2 {
         // 而是引入一个外围类的实例以调用外围类的实例方法
         // private Home2 home;
         private List<String> catNames = new ArrayList<>();
+        private Home2 home = new Home2();
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+            home.log(cat);
             catNames.add(cat.getName());
         }
 
