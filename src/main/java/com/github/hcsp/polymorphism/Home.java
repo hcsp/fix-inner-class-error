28571be0f40c92be39a96c580e8ee62ec1b39d63
tq13111 <63287795+tq13111@users.noreply.github.com>
2021-11-22 11:10:47 +0800

    内部类练习 (#209)
    内部类练习 (#209)

--- src/main/java/com/github/hcsp/polymorphism/Home.java
+++ src/main/java/com/github/hcsp/polymorphism/Home.java
@@ -21,9 +21,8 @@ public class Home {
     // 在这个类里会产生一个编译错误
     // 请思考一下为什么
     // 并将此类改写成非静态的内部类，以修复此问题
-    static class CatNameCollector implements Consumer<Cat> {
+     class CatNameCollector implements Consumer<Cat> {
         private List<String> catNames = new ArrayList<>();
-
         @Override
         public void accept(Cat cat) {
             log(cat);
