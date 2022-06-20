d80ac30bb372afbdc9950a75e9e9f1bcb5a997b4
yangyangsimons <97254476+yangyangsimons@users.noreply.github.com>
2022-06-20 19:13:44 +0100

    fix bugs of static classes acees to outer class (#224)
    fix bugs of static classes acees to outer class (#224)

--- src/main/java/com/github/hcsp/polymorphism/Home2.java
+++ src/main/java/com/github/hcsp/polymorphism/Home2.java
@@ -28,7 +28,8 @@ public class Home2 {
 
         @Override
         public void accept(Cat cat) {
-            log(cat);
+
+            new Home2().log(cat);
             catNames.add(cat.getName());
         }
 
